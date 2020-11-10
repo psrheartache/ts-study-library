149a1a657e1a2508b140dfb198ee1293da5d2950
HEartache_psR <chenmr.cyf@qq.com>
2020-11-10 10:14:49 +0800

    提交项目初始化测试代码

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: Yufeng CHEN
+ * @Date: 2020-11-10 10:00:38
+ * @LastEditors: Yufeng CHEN
+ * @LastEditTime: 2020-11-10 10:13:58
+ * @FilePath: /ts-study-library/src/index.ts
+ */
+let num: number = 123
+
+document.title = 'cyf'
